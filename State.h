cf4306ebc3092a5595ed0aa720f609f97210d109
Bá Lương <79621768+20020001-UET@users.noreply.github.com>
2021-04-27 23:04:05 +0700

    State

---
+++ State.h
@@ -0,0 +1,68 @@
+///State [Header]
+#ifndef STATE_H_
+#define STATE_H_
+
+///Include header
+#include "System.h"
+
+///State Type
+enum STATE_TYPE
+{
+    UNSET_STATE = -1,
+    MENU_STATE,
+    PLAYING_STATE,
+    HOW_TO_PLAY_STATE,
+    SETTING_STATE,
+    HIGHSCORE_STATE,
+    ABOUT_STATE,
+    RESUME_STATE,
+    SAVE_SETTING_STATE,
+    EXIT_GAME_STATE,
+    CLOSE_STATE,
+    RETURN_STATE,
+    EXIT_STATE,
+    STATE_TYPE_TOTAL
+};
+
+///State class
+class State
+{
+    public:
+        //Constructor:
+        State();
+        State(System* _system);
+
+        //Destructor:
+        virtual ~State();
+
+        ///main function:
+        //init:
+        virtual void init(System* _system) {};
+
+        //loop:
+        virtual void loop() = 0;
+
+        //render:
+        virtual void render() = 0;
+
+        //handle key event:
+        virtual void keyPressed(const int key_code) = 0;
+        virtual void keyReleased(const int key_code) = 0;
+
+        //close:
+        virtual void close() {};
+
+        //pull state function:
+        void pull(STATE_TYPE _request);
+        STATE_TYPE request();
+        void pulled();
+
+    protected:
+        ///The request State
+        STATE_TYPE requestState;
+
+        ///System
+        System* system;
+};
+
+#endif // STATE_H_
---
+++ StateManager.cpp
@@ -0,0 +1,183 @@
+///State Manager [Source]
+#include "StateManager.h"
+
+///Include header
+#include "MenuState.h"
+#include "PlayingState.h"
+#include "HowToPlayState.h"
+#include "SettingState.h"
+#include "HighScoreState.h"
+#include "AboutState.h"
+#include "ResumeState.h"
+#include "SaveSettingState.h"
+#include "ExitGameState.h"
+
+///State Manager class
+//Constructor:
+StateManager::StateManager()
+{
+    console = new Console("State Manager");
+    return;
+}
+
+//Destructor:
+StateManager::~StateManager()
+{
+    delete console;
+    console = NULL;
+
+    system = NULL;
+
+    return;
+}
+
+///main function:
+//init:
+void StateManager::init(System* _system)
+{
+    system = _system;
+
+    console->writeLine("Initialized State Manager");
+
+    add(new MenuState);
+
+    return;
+}
+
+//close:
+void StateManager::close()
+{
+    console->writeLine("Closing State Manager. . .");
+
+    while (!isEmpty())
+        remove();
+
+    return;
+}
+
+///state function:
+//add:
+void StateManager::add(State* state)
+{
+    states.push(state);
+    states.top()->init(system);
+    return;
+}
+
+//loop:
+void StateManager::loop()
+{
+    if (!isEmpty())
+        states.top()->loop();
+    return;
+}
+
+//render:
+void StateManager::render()
+{
+    if (!isEmpty())
+        states.top()->render();
+    return;
+}
+
+//handle key event:
+void StateManager::keyPressed(const int key_code)
+{
+    if (!isEmpty())
+        states.top()->keyPressed(key_code);
+    return;
+}
+
+void StateManager::keyReleased(const int key_code)
+{
+    if (!isEmpty())
+        states.top()->keyReleased(key_code);
+    return;
+}
+
+//remove:
+void StateManager::remove()
+{
+    if (!isEmpty())
+    {
+        //create a pointer to the last top element
+        State* tmpState = states.top();
+
+        states.top()->close();
+        states.pop();
+
+        //delete the old state
+        delete tmpState;
+        tmpState = NULL;
+    }
+    return;
+}
+
+//is the stack empty?
+bool StateManager::isEmpty()
+{
+    return states.empty();
+}
+
+//state request:
+bool StateManager::pullRequest()
+{
+    bool running = true;
+    if (!isEmpty())
+    {
+        if (states.top()->request() != UNSET_STATE)
+        {
+            STATE_TYPE stateType = states.top()->request();
+            states.top()->pulled();
+            switch (stateType)
+            {
+                case MENU_STATE:
+                    {
+                        while (!isEmpty())
+                            remove();
+                        add(new MenuState);
+                        break;
+                    }
+                case PLAYING_STATE:
+                    add(new PlayingState);
+                    break;
+                case HOW_TO_PLAY_STATE:
+                    add(new HowToPlayState);
+                    break;
+                case SETTING_STATE:
+                    add(new SettingState);
+                    break;
+                case HIGHSCORE_STATE:
+                    add(new HighScoreState);
+                    break;
+                case ABOUT_STATE:
+                    add(new AboutState);
+                    break;
+                case RESUME_STATE:
+                    add(new ResumeState);
+                    break;
+                case SAVE_SETTING_STATE:
+                    add(new SaveSettingState);
+                    break;
+                case EXIT_GAME_STATE:
+                    add(new ExitGameState);
+                    break;
+                case RETURN_STATE:
+                    {
+                        remove();
+                        remove();
+                        break;
+                    }
+                case CLOSE_STATE:
+                    remove();
+                    break;
+                case EXIT_STATE:
+                    running = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    return running;
+}
